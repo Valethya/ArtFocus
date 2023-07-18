837f2f10828d08246fb18b56177710089c625647
Valentina Rojas <valenthinarojasr@gmail.com>
2023-07-18 11:54:35 -0400

    cambiod en __dirname

--- Server/src/utils/util.js
+++ Server/src/utils/util.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from "url";
 import multer from "multer";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(dirname(__filename));
+const __dirname = dirname(dirname);
 
 const storage = (folder) => {
   return multer.diskStorage({
