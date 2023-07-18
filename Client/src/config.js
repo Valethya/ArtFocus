79a4b15ebe1151f0d01748e868c7e32b5171610a
Valentina Rojas <valenthinarojasr@gmail.com>
2023-07-18 11:18:19 -0400

    cambios en la url basedel cliente

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,3 +1,4 @@
 // const BASE_URL = "https://artfocus-production.up.railway.app";
-const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://artfocus.onrender.com";
 export default BASE_URL;
